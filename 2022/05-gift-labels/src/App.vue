591833f12e60bccaed56f628917e13530eabeb7a
siddharthasarma <sidduinevitable@gmail.com>
2022-12-17 09:26:18 +0530

    Solution for 05:Gift labels

---
+++ 2022/05-gift-labels/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="w-full h-full flex justify-center items-center">
+    <div>
+      <div class="w-full max-w-2xl p-4 rounded bg-red-400 text-white">
+        <div class="border-2 rounded flex flex-col md:flex-row md:items-center md:gap-8 p-4">
+          <img src="/assets/santa.svg" alt="" class="max-w-[12em]" />
+          <div>
+            <h1 class="font-serif text-4xl">Happy Holidays!</h1>
+            <div class="mt-4 bg-white text-black p-2 rounded">TO: {{ to }}</div>
+            <div class="mt-2 bg-white text-black p-2 rounded">FROM: {{ from }}</div>
+          </div>
+        </div>
+      </div>
+      <label for="to"
+        >To:
+        <input
+          id="to"
+          type="text"
+          class="
+            bg-gray-50
+            border border-gray-300
+            text-gray-900 text-sm
+            rounded-lg
+            focus:ring-blue-500 focus:border-blue-500
+            block
+            w-full
+            p-2
+          "
+          v-model="to"
+        />
+      </label>
+      <label for="from"
+        >From:
+        <input
+          id="from"
+          type="text"
+          class="
+            bg-gray-50
+            border border-gray-300
+            text-gray-900 text-sm
+            rounded-lg
+            focus:ring-blue-500 focus:border-blue-500
+            block
+            w-full
+            p-2
+          "
+          v-model="from"
+        />
+      </label>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// ...
+import { ref } from 'vue'
+const to = ref('')
+const from = ref('')
+</script>
