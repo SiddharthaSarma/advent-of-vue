73cd7a3b3531aedd159d81a787ba6757f2551d5b
siddharthasarma <sidduinevitable@gmail.com>
2022-12-17 08:44:48 +0530

    Solution for 04.Recursive tree

---
+++ 2022/04-recursive-tree/src/ChristmasTree.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ChristmasTree v-if="size > 1" :size="size - 1" />
+    <div class="flex flex-row justify-center">
+      <div
+        v-for="i in size"
+        :key="i"
+        class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    size: number
+  }>(),
+  {
+    size: 1,
+  }
+)
+</script>
