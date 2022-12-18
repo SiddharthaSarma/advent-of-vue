91e83b00defd6f4628e66c1872a83c41fce871a1
siddharthasarma <sidduinevitable@gmail.com>
2022-12-18 21:51:02 +0530

    06: Gift price comparison init solution

---
+++ 2022/06-compare-items/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref} from 'vue'
+import ItemSelect from './components/ItemSelect.vue'
+import ComparisonSummary from './components/ComparisonSummary.vue'
+const products = ref([])
+
+const getProducts = async() => {
+  const results = await fetch('https://dummyjson.com/products');
+  const json = await results.json()
+  products.value = json.products
+}
+getProducts()
+</script>
+
+<template>
+  <div class="w-full h-full flex flex-col gap-5 justify-center items-center">
+    <h1 class="text-4xl font-bold">Select items to compare</h1>
+    <div class="flex flex-col gap-5 justify-center">
+      <ItemSelect :products="products" />
+      <ItemSelect :products="products" />
+    </div>
+    <ComparisonSummary />
+  </div>
+</template>
---
+++ 2022/06-compare-items/src/components/ItemSelect.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+import { useItemComparison } from '../composables/itemComparison'
+
+const selected = ref()
+defineProps({
+  products: Array
+})
+</script>
+
+<template>
+  <select v-model="selected" class="p-2 border-2 border-gray-dark">
+    <option value="product.id" v-for="product in products" :key="product.id" >{{product.title}}</option>
+  </select>
+</template>
